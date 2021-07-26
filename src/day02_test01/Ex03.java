cba24bb7dcb8875b6ab6650790ffd24bf59b2495
sungtaek <ohbass95@gmail.com>
2021-07-26 23:04:38 +0900

    Ex03 복습

---
+++ src/day02_test01/Ex03.java
@@ -0,0 +1,21 @@
+package day02_test01;
+
+public class Ex03 {
+	
+	public static void main(String[] args) {
+		int num = 123;
+		char ch = 'd';
+//		작은 따옴표 문자 하나를 표현할 때 사용한다
+		
+		num = num +10;
+//		123을 입력한 상태에서 10이 더해졌다
+		ch = 'G';
+		System.out.println("num : " + num);
+		System.out.println("ch : " + ch);
+		
+//		변수의 숫자는 언제든지 변경이 가능하다
+//				문자 한글자를 입력할때에는 ''를 붙힌다
+		
+	}
+
+}
