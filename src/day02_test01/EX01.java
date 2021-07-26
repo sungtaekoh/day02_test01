c2faeb8454cfaee1c6a86115d9fc567d4098a5ed
sungtaek <ohbass95@gmail.com>
2021-07-26 22:58:13 +0900

    Ex0102 복습

--- src/day02_test01/EX01.java
+++ src/day02_test01/EX01.java
@@ -2,4 +2,47 @@ package day02_test01;
 
 public class EX01 {
 
+	public static void main(String[] args) {
+		
+	  /*
+	   * 변수 작서아방법
+	   * 자료형(type) 변수이름 : 아무거나 써도 상관없지만
+	   * 의미를 부여하는게 좋다 추후에 유추하기 쉽다
+	   * 
+	   * 자료형 변수이름 = 값
+	   * 
+	   * 변수 
+	   * 무언가(data)를 저장하기 위한 공간
+	   * 
+	   * 변수명 작성 규칙
+	   * 될 수있으면 영어로 작성한다
+	   * 변수명의 첫 글자는 소문자로 작성한다
+	   * 만약 합성어가 존재한다면 합성어의 첫글자는 대문자로 쓴다
+	   * ex)numSum/숫자의 합
+	   * 
+	   */
+		/*
+		 * 자료형
+		 * 정수 
+		 * short 2byte 음와 양수 표현가능
+		 * int 4byte 음수와 양수 표현 가능
+		 * 
+		 * 실수
+		 * float 4byte 음수와 양수표현가능
+		 * double 8byte 음수와 양수 표현가능
+		 * 
+		 * 문자 
+		 * byte 1byte 음수와 양수표현 가능
+		 * char 2byte 양수만 표현 가능/주로 문자표현
+		 * 
+		 * 같은 용량이라 하더라도 음수,양수에 따라 범위가 달라진다
+		 * 
+		 * 논리형
+		 * doolean 1byte(tre참 false거짓)만 표현 가능
+		 * 
+		 * 컴퓨터 단위 그냥 외우면다
+		 * bit byte kbyte mbyte gbyte
+		 */
+		 
+	}
 }
