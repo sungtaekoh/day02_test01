a96589d9782322336f75e855c8ed79d7173977bc
sungtaek <ohbass95@gmail.com>
2021-07-26 23:18:29 +0900

    Ex 04 복습

---
+++ src/day02_test01/Ex04.java
@@ -0,0 +1,25 @@
+package day02_test01;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		
+		char ch = 'A';
+		int num = 5, ret;
+		/*int = 5,ret;
+		 * int num = 5 는 num안에 5가 들어간다
+		 *  int = ret는 안에 뭐가 들어가 있는지 모른다
+		 */
+		ret = ch + num;
+		System.out.println("ret ="+ret);
+//		해당하는 숫자를 문자로 표현하겠습니다
+/*
+ * 자바에서는 모든 값을 유니코드로 표현한다
+ * 유니코드: 문자와 컴퓨터가 알아볼 수 있는 숫자를 
+ * 규칙을 통해 정해놓은것
+ * 
+ * 아스키 코드		
+ */
+	}
+	
+}
